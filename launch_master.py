a264c149e7bf284eb96872416dab86fe3533fe8c
Mickey Shaughnessy <mickeyshaughnessy@Mickey-Shaughnessys-MacBook-Pro-M1.local>
2022-12-20 13:28:19 -0700

    update listener

--- launch_master.py
+++ launch_master.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
-import requests
+import sys, requests
+
 
 def handle_speech_input(request):
     # Extract the audio data from the request
@@ -10,16 +11,13 @@ def handle_speech_input(request):
     audio = sr.AudioData(data=audio_data, sample_rate=44100, 
                          sample_width=2)
     text = r.recognize_google(audio)
+    print(text)
 
 
 if __name__ == "__main__":
-    port = int(sys.argv[1])
-    paced = int(sys.argv[2])
-    bidder = Bidder(p=port, budget=1000, price_paced=paced)
+    bot = Robot(p=port)
 
     app = falcon.App()
-    app.add_route('/bid', bidder)
-    app.add_route('/win', bidder)
-    app.add_route('/conv', bidder)
+    app.add_route('/listen', bot)
     httpd = simple_server.make_server('0.0.0.0', int(sys.argv[1]), app)
     httpd.serve_forever() 
